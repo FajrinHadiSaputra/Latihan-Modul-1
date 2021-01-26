992673af09374371603650cbb1cb3d642894ce7b
FajrinHadiSaputra <77818902+FajrinHadiSaputra@users.noreply.github.com>
2021-01-26 20:33:48 +0800

    fix latihan13

---
+++ Latihan13.py
@@ -0,0 +1,14 @@
+x=20
+y=60
+z=55
+
+if 
+    print('x berkisar antara 10 dan 30')
+
+if x
+    print('x bilangan paling besar')
+elif:
+    print('y bilangan paling besar')
+else:
+    print('z bilangan paling besar')
+    
